c4b2e0d9250c1cbfd6648c4cdbc0d61335777375
Dev-Emilio <emilio.notb@gmail.com>
2025-05-06 09:13:13 -0300

    Increase cod

--- Meus projetos/Ex95.py
+++ Meus projetos/Ex95.py
@@ -2,10 +2,11 @@
 jogador = dict()
 partidas = list()
 time = list()
-while True:
+while True: #CODIGO PARA ALIMENTAR
     jogador.clear()
     jogador['Nome'] = str(input('Nome do jogador: '))
     tot = int(input(f'Quantas partidas {jogador['Nome']} jogou? '))
+    partidas.clear()
     for c in range (0, tot):
         partidas.append(int(input(f'    Quantos gols na partida {c+1}? ')))
         jogador['Gols'] = partidas[:]
@@ -18,11 +19,27 @@ while True:
         print('ERRO! Por favor, digite apenas S ou N')
     if resp in 'N':
         break
+print('-='*30)#CODIGO PARA MOSTRAR
+print('COD ', end='')
+for i in jogador.keys():
+    print(f'{i:<15} ', end='')
+print()
 print('-'*40)
 for k, v in enumerate(time):
-    print(f'')
-
-print('-='*20)
-print(f'O jogador {jogador['Nome']} jogou {len(jogador['Gols'])} partidas')
-for i, v in enumerate(jogador['Gols']):
-    print(f'    => Na partida {i+1}, fez {v} gols')
+    print(f'{k:>2}  ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-'*40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    if busca == 999:
+        break
+    if busca > len(time):
+        print(f'ERRO! Não existe jogador com codigo {busca}')
+    else:
+        print(f' --- LEVANTAMENTO DO JOGADOR {time[busca]['Nome']}: ')
+        for i, g in enumerate(time[busca]['Gols']):
+            print(f'    No jogo {i+1} fez {g} gols')
+    print('-'*40)
+print('>>> VOLTE SEMPRE <<<')
